2a8f73ddc522a9ae093e59e42dd81589b14c5a2d
Minn <minn.wu@sinodynamic.com>
2020-02-13 11:12:10 +0800

    Update

--- Test/__Test__.appium.test.js
+++ Test/__Test__.appium.test.js
@@ -4,7 +4,14 @@ jasmine.DEFAULT_TIMEOUT_INTERVAL = 60000;
 const PORT = 4723;
 const config = {
   platformName: 'Android',
-  deviceName: 'Android Emulator',
-  app: './android/app/build/outputs/apk/app-debug.apk' // relative to root of project
+  deviceName: 'WUJ01JN0TW',
+  automationName: 'UiAutomator2',
+  app: 'D:/tng_consumer_4.1.3_dr_1220.apk' // relative to root of project
 };
 driver = wd.promiseChainRemote('localhost', PORT);
+beforeAll(async () => {
+  await driver.init(config);
+  await driver.sleep(2000); // wait for app to load
+})
+test('appium renders', async () => {
+});
